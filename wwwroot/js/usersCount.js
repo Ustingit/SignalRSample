f616ee5d1c726435229bc95384041cdb89c09469
yuryi.ustinovich <ustinhtc@gmail.com>
2023-08-24 17:46:48 +0300

    example with counter works fine

---
+++ wwwroot/js/usersCount.js
@@ -0,0 +1,25 @@
+﻿//declaring connection string
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect (kind of establishing two-way connection) (connect to methods that hub invokes aka receive notifications from hub)
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods (send notification to hub)
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+function fulfilled() {
+    console.log("connection succcessful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("connection refused");
+}
+
+//start connection
+connectionUserCount.start().then(fulfilled, rejected);
