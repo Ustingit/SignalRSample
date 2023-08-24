000f9a495773668b046e5ed93ff5e614d3974502
yuryi.ustinovich <ustinhtc@gmail.com>
2023-08-24 17:26:16 +0300

    add client side signal r library and backbone for the first method

---
+++ Hubs/UserHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+	public class UserHub : Hub
+	{
+		private static int _totalViews = 0;
+
+		public async Task NewWindowLoaded()
+		{
+			_totalViews++;
+			await Clients.All.SendAsync("updateTotalViews", _totalViews);
+		}
+
+
+	}
+}
